05f99f8d5d57b432f4edc670702d2733ce84bc65
Sanjarbek <sanjarbekabdurahmonov123@gmail.com>
2024-09-20 19:29:19 +0500

    Ready

--- storage/postgres/user.go
+++ storage/postgres/user.go
@@ -1,8 +1,11 @@
 package postgres
 
 import (
+	"context"
 	"database/sql"
+	"errors"
 	"log/slog"
+	pb "user/genproto/user"
 	"user/logs"
 	"user/storage"
 )
@@ -18,3 +21,44 @@ func NewUserRepo(DB *sql.DB) storage.IUserStorage {
 	return &UserRepo{DB: DB, Log: logs.NewLogger()}
 }
 
+func (u *UserRepo) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.Void, error) {
+
+	query := `INSERT INTO users (hh_id, first_name, last_name, password_hash, phone_number, gender, date_of_birth) VALUES ($1, $2, $3, $4, $5, $6, $7)`
+
+	err := u.DB.QueryRow(query, req.HhId, req.Firstname, req.Lastname, req.Password, req.Phone, req.Gender, req.DateOfBirth)
+	if err != nil {
+		u.Log.Error("Error while Registering", "error", err)
+		return nil, errors.ErrUnsupported
+	}
+
+	return &pb.Void{}, nil
+}
+
+func (u *UserRepo) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error) {
+
+	var user pb.LoginResponse
+
+	query := `SELECT id, role FROM users WHERE hh_id = $1 and password_hash = $2 and deleted_at IS NULL`
+
+	err := u.DB.QueryRow(query, req.HhId, req.Password).Scan(&user.HhId, &user.Role)
+	if err == sql.ErrNoRows {
+		u.Log.Error("No user found", "ID", req.HhId)
+		return nil, errors.New("no user found")
+	} else if err != nil {
+		u.Log.Error("Error getting user by ID", "err", err)
+		return nil, err
+	}
+
+	return &pb.LoginResponse{
+		HhId: user.HhId,
+		Role: user.Role,
+	}, nil
+}
+
+func (u *UserRepo) GetProfile(ctx context.Context, req *pb.GetProfileRequest) (*pb.GetProfileResponse, error) {
+	var user pb.GetProfileResponse
+
+	query := `SELECT hh_id, first_name, last_name, password_hash, phone_number, date_of_birth, gender`
+
+	err := u.DB.QueryRow(query, )
+}
--- storage/storage.go
+++ storage/storage.go
@@ -11,11 +11,11 @@ type IStorage interface {
 }
 
 type IUserStorage interface {
-	Register(context.Context, *pb.RegisterRequest) (*pb.RegisterResponse, error)
+	Register(context.Context, *pb.RegisterRequest) (*pb.Void, error)
 	Login(context.Context, *pb.LoginRequest) (*pb.LoginResponse, error)
 	GetProfile(context.Context, *pb.GetProfileRequest) (*pb.GetProfileResponse, error)
 	GetAllUsers(context.Context, *pb.GetAllUsersRequest) (*pb.GetAllUsersResponse, error)
-	UpdateProfile(context.Context, *pb.UpdateProfileRequest) (*pb.UpdateProfileResponse, error)
-	UpdateProfileAdmin(context.Context, *pb.UpdateProfileAdminRequest) (*pb.UpdateProfileAdminResponse, error)
-	DeleteProfile(context.Context, *pb.DeleteProfileRequest) (*pb.DeleteProfileResponse, error)
+	UpdateProfile(context.Context, *pb.UpdateProfileRequest) (*pb.Void, error)
+	UpdateProfileAdmin(context.Context, *pb.UpdateProfileAdminRequest) (*pb.Void, error)
+	DeleteProfile(context.Context, *pb.DeleteProfileRequest) (*pb.Void, error)
 }
